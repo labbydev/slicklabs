e45d7ec6ce1d37b97cd0789a556ac450148d0727
Lauren Burroughs <labyrwa@gmail.com>
2017-04-18 22:40:30 -0500

    add scroll indicator on hero

--- styleguide/src/content/assets/js/site.js
+++ styleguide/src/content/assets/js/site.js
@@ -11,14 +11,45 @@
 
 (function ($) {
 
-  Drupal.behaviors.site = {
+  // Check tranparent buttons against images for contrast
+  console.log('background check is running');
+  BackgroundCheck.init({
+    targets: '.button--transparent',
+    images: '.has-bkg-img'
+  });
+
+  Drupal.behaviors.scroll_links = {
     attach: function (context, settings) {
-      // Check tranparent buttons against images for contrast
-      BackgroundCheck.init({
-        targets: '.button--transparent'
+      var htmlBody = $('html, body');
+
+      // Get the window and window width.
+      var $window = $(window);
+      var window_width = $window.width();
+
+      // Debounce function to keep scroll and resize events from firing to frequently.
+      function debounce(method, delay) {
+        clearTimeout(method._tId);
+        method._tId= setTimeout(function(){
+          method();
+        }, delay);
+      }
+
+      // Scrollto Links
+      var scrollLink = $(".link--scroll");
+
+      scrollLink.click(function(event) {
+        // Prevent default anchor behavior so scrolling can animate.
+        event.preventDefault();
+
+        // Get the destination ID from the href.
+        var destinationLink = $(this).attr('href');
+        var destinationOffset = $(destinationLink).offset().top;
+
+        // Scroll to the destination.
+        htmlBody.animate({
+          scrollTop: destinationOffset
+        }, 500);
       });
-      // Reprocess
-      BackgroundCheck.refresh(target);
     }
   };
 
