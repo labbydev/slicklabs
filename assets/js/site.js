f6403ae3ce47a07c6444c0b961cb9b8b73ed0551
Lauren Burroughs <burroughs@palantir.net>
2017-04-21 09:57:48 -0500

    Updated with Butler - [timestamp]

--- assets/js/site.js
+++ assets/js/site.js
@@ -17,6 +17,11 @@
     images: '.has-bkg-img'
   });
 
+  Drupal.behaviors.custom_waypoints = {
+    attach: function (context, settings) {
+    }
+  };
+
   Drupal.behaviors.input_filled = {
     attach: function (context, settings) {
       $('input').blur(function(){
@@ -28,5 +33,34 @@
       });
     }
   };
+
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var navbarHeight = $('header').outerHeight();
+
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop){
+      $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+      $('header').removeClass('nav-up').addClass('nav-down');
+    }
+    lastScrollTop = st;
+  }
 
 })(jQuery, Drupal);
